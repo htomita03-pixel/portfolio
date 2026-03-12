956c4239be745840218352000ebaa8e9cde907df
htomita03-pixel <h.tomita03@gmail.com>
2026-03-12 14:28:07 +0900

    ポートフォリオサイトのTOPページ用JSを更新アップデートしました

--- scripts/script.js
+++ scripts/script.js
@@ -4,30 +4,49 @@
 //イベントを追加（HTMLが読み込み終わったら関数処理実行
 document.addEventListener('DOMContentLoaded', function () {
     // ドロワークローズ時の３本線アイコンを取得
-    const openBtn = document.getElementById('open_btn');
+    const openBtn = document.getElementById('js-open-btn');
     //追加分１　ドロワーオープン時のバツアイコンを取得　確実に読み込ませたいのでこちらにもいれておく
-    const closeBtn = document.getElementById('close_btn');
+    const closeBtn = document.getElementById('js-close-btn');
     // 追加分２　ドロワー本体の取得
-    const drawer = document.getElementById('drawer_menu');
+    const drawer = document.getElementById('js-drawer-menu');
     // 追加分４　オーバーレイ用タグの取得
-    const overLay = document.getElementById('over_lay');
+    const overlay = document.getElementById('js-over-lay');
 
+    if (!openBtn || !closeBtn || !drawer) {
+        return;
+    }
 
     openBtn.addEventListener('click', function () {
         drawer.classList.add('show');
-        overLay.classList.add('show');
+        overlay.classList.add('show');
     });
 
     closeBtn.addEventListener('click', function () {
         drawer.classList.remove('show');
-        overLay.classList.remove('show');
+        overlay.classList.remove('show');
     });
 
-    overLay.addEventListener('click', function () {
+    overlay.addEventListener('click', function () {
         drawer.classList.remove('show');
-        overLay.classList.remove('show');
+        overlay.classList.remove('show');
     });
- 
+    // PC → スマホ切替時に .show が残っていると一瞬出るのでクラスを強制的に外す
+    window.addEventListener('resize', () => {
+        if (window.innerWidth >= 768) {
+            drawer.classList.remove('show');
+            overlay.classList.remove('show');
+        }
+    });
 });
 
+//TOPを開いたときにまずは全体に名前とイメージをオーバーレイさせておく
 
+//初期画面
+//画像・CSS・動画など、すべての読み込みが終わったら処理開始
+window.addEventListener("load", () => {
+    const loading = document.getElementById("js-over-wrap");
+
+    setTimeout(function () {
+        loading.classList.add("hide");
+    }, 2000);
+});
